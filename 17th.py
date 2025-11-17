59e14988df7df9035e787ba3b0f95ca3e11d3913
charlie-the-fraser <charliefraser200919854@gmail.com>
2025-11-17 10:13:09 +0000

    Create 17th.py

---
+++ 17th.py
@@ -0,0 +1,7 @@
+energy = ["high", "higher", "low", "highest", "medium"]
+usernames = ["theo.py", "charlie_the_fraser", "natethanial", "johnUsername", "67"]
+steps = [40000000, 67, 69, 420, 99999999999999999999999999, 2]
+
+print(energy)
+print(usernames[0], usernames[-1])
+print(steps[round((len(steps)) / 2)])
