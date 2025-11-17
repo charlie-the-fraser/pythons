302744fa647e7136c7d8887765e8ce015cced213
charlie-the-fraser <charliefraser200919854@gmail.com>
2025-11-17 10:50:12 +0000

    Update 17th.py

--- 17th.py
+++ 17th.py
@@ -5,3 +5,21 @@ steps = [40000000, 67, 69, 420, 99999999999999999999999999, 2]
 print(energy)
 print(usernames[0], usernames[-1])
 print(steps[round((len(steps)) / 2)])
+
+listA = [1, 2, 3]
+listB = [1, "2", 3.0]
+
+print(listB)
+
+notifications = [99, 28, 55, 40, 60, 22, 18]
+
+maxs = max(notifications)
+lowest = min(notifications)
+
+
+print(f"the largest is {maxs}, and the lowest is {lowest}")
+
+total = sum(notifications)
+print(total)
+average = total / len(notifications)
+print(average)
